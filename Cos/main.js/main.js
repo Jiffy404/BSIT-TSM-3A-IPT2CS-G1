f9410963f70466d08c19b6fee4d5ca08467bbed6
Christine Ann Cos <ca.cos00192@student.tsu.edu.ph>
2024-03-09 12:34:33 +0800

    With My Portfolio

---
+++ Cos/main.js/main.js
@@ -0,0 +1,12 @@
+
+        // JavaScript to handle smooth scrolling behavior
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+   
